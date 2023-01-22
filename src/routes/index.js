c06433b8ac30cfd1748655cefae4bd0d51a60135
Serhat SARI <serhhatsari@gmail.com>
2023-01-22 17:18:02 +0300

    updated router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const routes = require('express').Router();
 const auth = require('./auth');
 const post = require('./post');
-const verifyToken = require('../middlewares/verifyToken');
+const comment = require('./comment');
+const { verifyToken } = require('../middlewares/verifyToken');
 
 routes.use('/', auth);
-routes.use('/', verifyToken.verifyToken, post);
+routes.use('/', verifyToken, post);
+routes.use('/', verifyToken, comment)
 
 module.exports = routes;
